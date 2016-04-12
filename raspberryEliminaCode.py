6cd9b4d6b1c5e021d4637d356154fffb3642864b
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2016-04-12 17:19:29 +0200

    RASPBERRY: contatore procede correttamente

--- raspberryEliminaCode.py
+++ raspberryEliminaCode.py
@@ -1,39 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time, requests, json, urllib3
 from firebase import firebase
 
-
 pinBottone = 17 #6 pin basso
 loop = True
-#numeroServito = 0
 http = urllib3.PoolManager()
 url = 'http://elisapessa.altervista.org/eliminaCode/eliminaCode.php'
 urlLogin = 'http://gmail.com'
 
-#temporaneeee
-responseText = 'eliminaCodde = {numeroServito: 5, utimoNumeroPrenotato: 12}'
-
-
+test = True
 
 #FIEBASE
 urlDB = 'https://eliminacode.firebaseio.com'
 firebase = firebase.FirebaseApplication(urlDB, None)
 
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pinBottone, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-
-
-
-
-
-
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(pinBottone, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def recuperaNumeriClienti():
     #QUI SI DOVRANNO RECUPERARE I DATI IN FORMATO JSON CO HTTP
@@ -41,63 +26,54 @@ def recuperaNumeriClienti():
     #responseText = responseObject.text
     variabili = json.loads(responseText)
     numeroServito = variabili['numeroServito']
-    utimoNumeroPrenotato = variabili ('utimoNumeroPrenotato')
+    ultimoNumeroPrenotato = variabili ('ultimoNumeroPrenotato')
     print ('numeroServito ', numeroServito)
-    print ('utimoNumeroPrenotato ', utimoNumeroPrenotato)
-    return numeroServito, utimoNumeroPrenotatovariabili
-
-'''
-
-def sliceVariabili():
-    #inizioNumServito = responseText.find(chiaveNumeroServito) + len(chiaveNumeroServito) + 2
-    variabili = splitlines(responseText)
-    print(variabili)
-
+    print ('utimoNumeroPrenotato ', ultimoNumeroPrenotato)
     return numeroServito, ultimoNumeroPrenotato
-'''
-
-'''
-#chiamata HTTP
-def recuperoNumeroServito():
-    paginaHTTP = http.request('GET', url)
-    contenutoPagina = paginaHTTP.data
-    return contenutoPagina
-'''
 
 def determinaSeIlPulsanteEPremuto():
-    if (GPIO.input(pinBottone) == False):
-        numeroServito = recuperaNumeriClienti()
-        #numeroServito += 1
+    while (test == True):
+    #if (GPIO.input(pinBottone) == False):
         print('ho premuto il pulsante')
-        print(numeroServito)
+        #preleva la data per metterti nella giusta tabella del database
+        dataOdierna = time.strftime('%d%m%Y')
+        print(dataOdierna)
+        numeroServito, ultimoPrenotato, analogico = leggoDaFirebase(dataOdierna)
+        print('numeroServito ', numeroServito)
+        print ('ultimoPrenotato ', ultimoPrenotato)
+        aggiornaFirebase(ultimoPrenotato, analogico, dataOdierna)
+        #stampa il biglietto
+        calcolaAttesa(numeroServito, ultimoPrenotato)
+        #eventualmente invia i dati dell'utente
+        #determinaSeIlPulsanteEPremuto()
         time.sleep(0.2)
 
-def leggoDaFirebase():
+def leggoDaFirebase(dataOdierna):
     jsonRispostaFirebase = firebase.get(dataOdierna, None)
-    numeroServito = jsonRispostaFirebase['numeroServito']
-    ultimoNumeroPrenotato = jsonRispostaFirebase['ultimoNumeroPrenotato']
-    return numeroServito, ultimoNumeroPrenotato
+    numeroServito =  int(jsonRispostaFirebase['numeroServito'])
+    ultimoPrenotato = int(jsonRispostaFirebase['ultimoNumeroPrenotato'])
+    analogico = int(jsonRispostaFirebase['analogico'])
+    return numeroServito, ultimoPrenotato, analogico
 
-def aggiornaFirebase(numeroServito, ultimoNumeroPrenotato):
+def aggiornaFirebase(ultimoPrenotato, analogico, dataOdierna):
     print('inizio aggiorna')
     urlUpdate = urlDB + '/' + dataOdierna
-    result = firebase.patch(urlUpdate, data={'ultimoNumeroPrenotato':str(int(ultimoNumeroPrenotato) + 1)})
-    print(result)
+    #ultimoNumeroPrenotato = ultimoNumeroPrenotato+1
+    print(ultimoPrenotato)
+    resultUltimo = firebase.patch(urlUpdate, data={'ultimoNumeroPrenotato': str(ultimoPrenotato + 1)})
+    resultAnalogico = firebase.patch(urlUpdate, data={'analogico':str(int(analogico) + 1)})
+    print (resultUltimo)
+    print(resultAnalogico)
+
+def calcolaAttesa(numeroServito, ultimoNumeroPrenotato):
+    codaPersone = ultimoNumeroPrenotato - numeroServito
+    jsonRispostaFirebaseTempistiche = firebase.get('TempiAttesa', None)
+    mediaPaziente = jsonRispostaFirebaseTempistiche['mediaPaziente']
+    tempoAttesa = mediaPaziente * codaPersone
+    print('Hai ', codaPersone, ' persone davanti a te.')
+    print('Il tempo stimato di attesa è di ', tempoAttesa)
 
 
 print('Avvio... \n')
 while loop == True:
-    #preleva la data per metterti nella giusta tabella del database
-    dataOdierna = time.strftime('%d%m%Y')
-    #print(dataOdierna)
-    #preleva dati dal db
-    numeroServito, ultimoNumeroPrenotato = leggoDaFirebase()
-    print('numeroServito ', numeroServito)
-    print ('ultimoNumeroPrenotato ', ultimoNumeroPrenotato)
-    #invia aggiornamenti contatori
-    aggiornaFirebase(numeroServito, ultimoNumeroPrenotato)
-    #stampa il biglietto
-    #eventualmente invia i dati dell'utente
-
-    #determinaSeIlPulsanteEPremuto()
-    loop = False
+    determinaSeIlPulsanteEPremuto()
